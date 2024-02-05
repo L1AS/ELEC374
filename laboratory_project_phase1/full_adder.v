81265c551a056e4c8bb6976329d50bc818a51cdd
alteredsi <shenghaojin@outlook.com>
2024-02-05 16:53:42 -0500

    Completed the logic of full adder, discarded the use of half adder.v

--- laboratory_project_phase1/full_adder.v
+++ laboratory_project_phase1/full_adder.v
@@ -1,8 +1,15 @@
 module full_adder (
-    input a, b, Cin, 
-    output sum, Cout
+    input in_A, in_B, Cin, 
+    output out_Sum, Cout
 );
-wire 
 
-half_adder ha1(.a(a), .b(b), .sum())
+wire w1, w2, w3
+
+assign w1 = in_A ^ in_B; // in_A XOR in_B
+assign w2 = w1 & Cin; // w1 AND carry in (equivalent to (in_A XOR in_B) AND Cin)
+assign w3 = in_A & in_B; // in_A AND in_B
+
+assign out_Sum = w1 ^ Cin; // equivalent to in_A XOR in_B XOR Cin
+assign Cout = w2 | w3;
+
 endmodule
