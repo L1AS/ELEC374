cca7f2657564c80e1f59cb21a87b50fc2e1acf6e
alteredsi <shenghaojin@outlook.com>
2024-02-05 16:56:23 -0500

    Added a generate for loop to iteratively create instances for the full adder

--- laboratory_project_phase1/cla_gen.v
+++ laboratory_project_phase1/cla_gen.v
@@ -8,7 +8,18 @@ wire [WIDTH:0] wire_C; // carry intermediate
 wire [WIDTH-1:0] wire_G, wire_P, w_SUM; // carry generate, carry propagate, sum
 
 // full adder, consider use generate function in verilog
-
-// G = A * B (a.k.a A AND B)
+gen var i;
+generate
+    for (i = 0, i < WIDTH, i = i + 1) 
+    begin
+        full_adder full_adder_i (
+            .in_A (in_A[i]),
+            .in_B (in_B[i]),
+            .Cin (wire_C[i]),
+            .out_Sum (w_SUM[i])
+            .Cout()
+        );
+    end
+// Generate Terms G = A * B (a.k.a A AND B)
 assign G = in_A & in_B; 
 // P = A XOR B
