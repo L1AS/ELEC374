6ffcf6eaefdd4adbcd36d0d759c9148547e0ae99
alteredsi <shenghaojin@outlook.com>
2024-02-06 16:02:29 -0500

    Revise comments

--- laboratory_project_phase1/half_adder.v
+++ laboratory_project_phase1/half_adder.v
@@ -3,6 +3,7 @@ module half_adder(
   output out_Sum, Cout
 );
 // S = A XOR B
-  xor x1(out_Sum, in_A, in_B);
-  and a1(Cout, in_A, in_B);
+xor x1(out_Sum, in_A, in_B);
+// C = A AND B
+and a1(Cout, in_A, in_B);
 endmodule
