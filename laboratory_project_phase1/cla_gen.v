436bcec38822b38736384a56376d3ced11870e5e
alteredsi <shenghaojin@outlook.com>
2024-02-12 18:36:04 -0500

    Made both inout and output as signed ports

--- laboratory_project_phase1/cla_gen.v
+++ laboratory_project_phase1/cla_gen.v
@@ -1,25 +1,25 @@
 module cla_gen#(parameter WIDTH = 32)(
-    input [WIDTH-1:0] in_add1, in_add2,
+    input signed [WIDTH-1:0] in_add1, in_add2,
     input Cin,  // carry in
-    output [WIDTH-1:0] out_Sum
+    output signed [WIDTH-1:0] out_Sum
 );
 
 	wire [WIDTH:0] wire_C; // carry intermediate
-	wire [WIDTH-1:0] wire_G, wire_P, wire_SUM; // carry generate, carry propagate, sum
-
+	wire [WIDTH-1:0] wire_G, wire_P; // carry generate, carry propagate
+	wire signed [WIDTH-1:0] wire_SUM; // sum
 	// full adder, consider use generate function in verilog
 	genvar i;
 	generate
-		 for (i = 0; i < WIDTH; i = i + 1) 
-		 begin: gen_sum
-			  full_adder full_adder_i (
-					.in_A (in_add1[i]),
-					.in_B (in_add2[i]),
-					.Cin (wire_C[i]),
-					.out_Sum (wire_SUM[i]),
-					.Cout()
-			  );
-		 end
+		for (i = 0; i < WIDTH; i = i + 1) 
+		begin: gen_sum
+			full_adder full_adder_i (
+				.in_A (in_add1[i]),
+				.in_B (in_add2[i]),
+				.Cin (wire_C[i]),
+				.out_Sum (wire_SUM[i]),
+				.Cout()
+			);
+		end
 	endgenerate
 
 	assign wire_C[0] = 1'b0;
@@ -27,12 +27,12 @@ module cla_gen#(parameter WIDTH = 32)(
 	// P = A XOR B or A OR B
 	genvar j;
 	generate
-		 for (j = 0; j < WIDTH; j = j + 1)
-		 begin: gen_genrate_terms
-			  assign wire_G[j] = in_add1[j] & in_add2[j];
-			  assign wire_P[j] = in_add1[j] ^ in_add2[j];
-			  assign wire_C[j+1] = wire_G[j] | (wire_P[j] & wire_C[j]);    
-		 end
+		for (j = 0; j < WIDTH; j = j + 1)
+		begin: gen_genrate_terms
+			assign wire_G[j] = in_add1[j] & in_add2[j];
+			assign wire_P[j] = in_add1[j] ^ in_add2[j];
+			assign wire_C[j+1] = wire_G[j] | (wire_P[j] & wire_C[j]);    
+		end
 	endgenerate
 
 	// Concatenation
