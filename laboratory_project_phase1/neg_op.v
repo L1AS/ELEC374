b19d707411d16832fdc049ca7989839f82403853
Tristan <19tja3@queensu.ca>
2024-02-13 13:51:55 -0500

    updated negate function

--- laboratory_project_phase1/neg_op.v
+++ laboratory_project_phase1/neg_op.v
@@ -1,8 +1,12 @@
 module neg_op (
-  input wire[31:0] Breg,
-  output wire[31:0] Negout
+  input wire[31:0] B_reg,
+  output wire[31:0] neg_out
 );
-
-  assign Negout = -Breg;
-  
+  reg[31:0] temp;
+  always@(*) begin
+    temp = B_reg;
+    not_op not(B_reg, temp);
+    add_op add(32'd1, temp, temp);
+  end
+  assign neg_out = temp;
 endmodule
