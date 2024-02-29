9bad682dc27bd9030a0970124411ce91bd19f34d
Kangyu Zhao <18kz2@queensu.ca>
2024-02-29 09:48:21 -0500

    fixed shra_op and updated report

--- Phase1/laboratory_project_phase1/ALU/shra_op.v
+++ Phase1/laboratory_project_phase1/ALU/shra_op.v
@@ -1,6 +1,6 @@
 module shra_op(
-  output [31:0] shra_out,
-  input [31:0] A_reg, B_reg
+  output signed [31:0] shra_out,
+  input signed [31:0] A_reg, B_reg
 );
 
   assign shra_out = A_reg >>> B_reg;
