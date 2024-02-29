15f33dabbc8060c07078fe88aaf4f0813c336f3b
alteredsi <shenghaojin@outlook.com>
2024-02-29 02:04:45 -0500

    Clean up the add_op to make bit stage cell serves as a standalone part

--- Phase1/laboratory_project_phase1/ALU/add_op.v
+++ Phase1/laboratory_project_phase1/ALU/add_op.v
@@ -1,14 +1,18 @@
 module add_op#(parameter WIDTH = 32)(
 	output signed [WIDTH-1:0] add_out,
+	// output [WIDTH:0] Cout
 	input signed [WIDTH-1:0] A_reg, B_reg,
 	input Cin  // carry in
-	// output [WIDTH:0] Cout
 );
 
 	wire [WIDTH:0] wire_C; // carry intermediate
 	wire [WIDTH-1:0] wire_G, wire_P; // carry generate, carry propagate
 	wire signed [WIDTH-1:0] wire_SUM; // sum
-	// full adder, consider use generate function in verilog
+
+	assign wire_C[0] = Cin;
+
+	// use bit cell logic to generate sum, carry out, 
+	// generate term, and propagate term repsectively
 	genvar i;
 	generate
 		for (i = 0; i < WIDTH; i = i + 1) 
@@ -17,26 +21,15 @@ module add_op#(parameter WIDTH = 32)(
 				.in_A (A_reg[i]),
 				.in_B (B_reg[i]),
 				.Cin (wire_C[i]),
+				.out_Propagate(wire_P[i]),
+				.out_Generate(wire_G[i]),
 				.out_Sum (wire_SUM[i]),
-				.Cout()
+				.Cout(wire_C[i+1])
 			);
 		end
 	endgenerate
 
-	assign wire_C[0] = Cin;
-	// Generate Terms G = A * B (a.k.a A AND B)
-	// P = A XOR B or A OR B
-	genvar j;
-	generate
-		for (j = 0; j < WIDTH; j = j + 1)
-		begin: gen_genrate_terms
-			assign wire_G[j] = A_reg[j] & B_reg[j];
-			assign wire_P[j] = A_reg[j] ^ B_reg[j];
-			assign wire_C[j+1] = wire_G[j] | (wire_P[j] & wire_C[j]);    
-		end
-	endgenerate
-
-	// Assign cla result and carry out
+	// Assign adder result and carry out
 	assign add_out = wire_SUM [WIDTH-1:0];
 	// assign Cout = wire_C [WIDTH:0];
 
