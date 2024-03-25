fe9a0f1d7d215a621bca41670afba693cebb95c4
Tristan Alderson <19tja3@queensu.ca>
2024-03-24 21:57:35 -0400

    updates from testing

--- Phase2/QuartusProject/TESTBENCHES/general_tb.v
+++ Phase2/QuartusProject/TESTBENCHES/general_tb.v
@@ -1,12 +1,13 @@
 `timescale 1ns / 10ps
 
 module general_tb;
-    reg[31:0] outPortData;                  // output.
-    reg CONFF_out, CONin;                   // branch logic signals
+    wire[31:0] outPortData;                  // output.
+    wire CONFF_out;
+    reg CONin;                   // branch logic signals
     reg[31:0] inPortDataIn;               // input.
     reg clock, clear,                     // control signals.
           Gra, Grb, Grc, Rin, Rout, BAout,  // control signals for IR
-          PCout, IncPC, PCin, IRin,          // PC and IR signals.
+          PCout_en, IncPC, PC_en, IRin,          // PC and IR signals.
           Yin, HIout, HIin, LOout, LOin,    // datapath MUX signals.
           Cout, Zhighout, Zlowout, Zin,     //
           MDRout, MDRin, MARin,             // Mem Data Interface signals.
@@ -17,7 +18,9 @@ module general_tb;
 
     // State definitions
     parameter Default = 4'b0000, T0 = 4'b0001, T1 = 4'b0010, T2 = 4'b0011, 
-              T3 = 4'b0100, T4 = 4'b0101, T5 = 4'b0111, T6 = 4'b1000, T7 = 4'b1001;
+              T3 = 4'b0100, T4 = 4'b0101, T5 = 4'b0111, T6 = 4'b1000, T7 = 4'b1001,
+              memWait = 4'b1111;
+              
     
     reg [3:0] Present_state = Default;
 
@@ -28,7 +31,7 @@ module general_tb;
         .inPortDataIn(inPortDataIn),                                            // input.                                
         .clock(clock), .clear(clear),                                           // control signals.
         .Gra(Gra), .Grb(Grb), .Grc(Grc), .Rin(Rin), .Rout(Rout), .BAout(BAout), // control signals for IR
-        .PCout(PCout), .IncPC(IncPC), .PCin(PCin), .IRin(IRin),                 // PC and IR signals.
+        .PCout_en(PCout_en), .IncPC(IncPC), .PC_en(PC_en), .IR_en(IRin),                 // PC and IR signals.
         .Yin(Yin), .HIout(HIout), .HIin(HIin), .LOout(LOout), .LOin(LOin),      // datapath MUX signals.
         .Cout(Cout), .Zhighout(Zhighout), .Zlowout(Zlowout), .Zin(Zin),         //
         .MDRout(MDRout), .MDRin(MDRin), .MARin(MARin),                          // Mem Data Interface signals.
@@ -51,7 +54,8 @@ module general_tb;
             Default: Present_state = T0;
             T0: Present_state = T1;
             T1: Present_state = T2;
-            T2: Present_state = T3;
+            T2: Present_state = memWait;
+            memWait: Present_state = T3;
             T3: Present_state = T4;
             T4: Present_state = T5;
             T5: Present_state = T6;
@@ -64,12 +68,11 @@ module general_tb;
     always @(Present_state) begin
         case (Present_state)
                 Default: begin
-                    outPortData <= 0;                                       // output.
                     inPortDataIn <= 0;                                      // input.
-                    clock <= 0; clear <= 0;                                 // control signals.
+                    clock <= 0; clear <= 1;                                 // control signals.
                     Gra <= 0; Grb <= 0; Grc <= 0;                           // control signals for IR
                     Rin <= 0; Rout <= 0; BAout <= 0;                        //
-                    PCout <= 0; IncPC <= 0; PCin <= 0; IRin <= 0;           // PC and IR signals.
+                    PCout_en <= 0; IncPC <= 0; PC_en <= 0; IRin <= 0;           // PC and IR signals.
                     Yin <= 0; HIout <= 0; HIin <= 0; LOout <= 0; LOin <= 0; // datapath MUX signals.
                     Cout <= 0; Zhighout <= 0; Zlowout <= 0; Zin <= 0;       //
                     MDRout <= 0; MDRin <= 0; MARin <= 0;                    // Mem Data Interface signals.
@@ -78,14 +81,18 @@ module general_tb;
                     opcode <= 5'b11010;                                     // assert nop
             end
             T0: begin // 1
-				PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1;    // prepare for increment PC via ALU
+                clear <= 0;
+				PCout_en <= 1; MARin <= 1; IncPC <= 1; Zin <= 1;    // prepare for increment PC via ALU
             end
             T1: begin //2
-				PCout <= 0; MARin <= 0; IncPC <= 0; Zin <= 0;
-                Zlowout <= 1; PCin <= 1; memRead <= 1; MDRin <= 1;	// PC incremented (taking value calculated in Z), read IR content from memory?			
+				PCout_en <= 0; MARin <= 0; IncPC <= 0; Zin <= 0;
+                Zlowout <= 1; PC_en <= 1; memRead <= 1; MDRin <= 1;	// PC incremented (taking value calculated in Z), read IR content from memory?			
+            end
+            memWait: begin
+                
             end
             T2: begin //3
-				Zlowout <= 0; PCin <= 0; memRead <= 0; MDRin <= 0;
+				Zlowout <= 0; PC_en <= 0; memRead <= 0; MDRin <= 0;
                 MDRout <= 1; IRin <= 1; // assert content from memory to IR
             end
             T3: begin //4
