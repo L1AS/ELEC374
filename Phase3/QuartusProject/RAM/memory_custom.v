4e2abaf04ab1b524f331bbf8f5eddb89b076e768
Shenghao Jin <18sj33@queensu.ca>
2024-04-05 01:59:02 -0400

    Signed-off-by: Shenghao Jin <18sj33@queensu.ca>

--- Phase3/QuartusProject/RAM/memory_custom.v
+++ Phase3/QuartusProject/RAM/memory_custom.v
@@ -1,21 +1,27 @@
 module memory_custom #(
     parameter DATA_WIDTH = 32,
     parameter ADDR_WIDTH = 9,
-    parameter MEM_DEPTH = 512
+    parameter MEM_DEPTH = 512  // 2**9
 )(
-    output reg [DATA_WIDTH-1:0] data_out,
+//    output reg [DATA_WIDTH-1:0] data_out,
+	output [(DATA_WIDTH-1):0] data_out,
 //    output reg done,                            // Signal to indicate operation completion
 //    input wire clk,                             // Still need a clock for the 'done' signaling
-    input wire [ADDR_WIDTH-1:0] addr,
-    input wire [DATA_WIDTH-1:0] data_in,
-    input wire write_enable,
-    input wire read_enable
+    input [(ADDR_WIDTH-1):0] addr,
+    input [(DATA_WIDTH-1):0] data_in,
+    input write_enable,
+    input read_enable
 );
-
-    reg [DATA_WIDTH-1:0] memory [0:MEM_DEPTH-1];
+	
+	// Declare the RAM variable
+//    reg [DATA_WIDTH-1:0] memory [0:MEM_DEPTH-1];
+	reg [DATA_WIDTH-1:0] memory [MEM_DEPTH-1:0];
+	
+	// Variable to hold the registered read address
+	reg [ADDR_WIDTH-1:0] addr_reg;
 
     initial begin
-        $readmemh("C:/Users/trist/OneDrive/Documents/GitHub/ELEC374/Phase3/QuartusProject/asm_HEX_Phase3", memory);
+        $readmemh("asm_HEX_Phase3", memory);
     end
     // Asynchronous Write Operation
     always @(write_enable, addr, data_in) begin
@@ -32,7 +38,8 @@ module memory_custom #(
     // Asynchronous Read Operation
     always @(read_enable, addr) begin
         if (read_enable) begin
-            data_out <= memory[addr];
+			addr_reg <= addr;
+//            data_out <= memory[addr];
             // Indicate that read operation is done in the next clock cycle
             // This is a compromise for synthesisable code
 //            done <= 1'b0; // Ensure done is initially low
@@ -40,7 +47,7 @@ module memory_custom #(
 //            done <= 1'b1; // Set done high briefly
         end
     end
-
+	assign data_out = memory[addr_reg];
 //    // Reset 'done' signal on clock edge
 //    always @(posedge clk) begin
 //        done <= 1'b0;
